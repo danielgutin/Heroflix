0eb2b854fe9b3f57aba76654894c1925f076f4ed
Daniel <danielgutin1995@gmail.com>
2019-03-18 14:33:17 +0200

    finished app

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import './Favorites.css';
 import './media.css';
+import empty from '../../assets/empty.png';
 
 //Redux Stuff.
 import { connect } from 'react-redux';
@@ -16,7 +17,14 @@ const Favorites = (props) => {
   return (
     <div className='Favorites page'>
       {
-        favorites.map((movie, i) => <MovieItem key={i} movie={movie} />)
+        favorites.length > 0 
+          ? favorites.map((movie, i) => <MovieItem key={i} movie={movie} />)
+          : (
+              <div className="Favorites-empty">
+                <img src={empty} alt="Empty"/>
+                <span>You have not added a favorite movie yet</span>
+              </div>
+            )
       }
     </div>
   )
